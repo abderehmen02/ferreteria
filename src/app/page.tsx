38b1a60491a98019206148a333751080375c04c1
abderrahmane <ilahalaha0987@gmail.com>
2024-06-15 14:11:50 +0100

    integrating the about features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { AppHero } from "./_components/hero";
 import { CategoriesSection } from "./_components/categories";
+import { AboutUs } from "./_components/aboutUs";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center ">
     <AppHero/>
     <CategoriesSection/>
+    <AboutUs/>
     </main>
   );
 }
