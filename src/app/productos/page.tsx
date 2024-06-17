1c24e7af9b15c70900c36ac514fdae25de4547bb
abderrahmane <ilahalaha0987@gmail.com>
2024-06-17 18:58:09 +0100

    wraping the filtered products in a suspense

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -3,6 +3,7 @@ import { ProductsHero } from "./_components/hero"
 import { DisplayAnimation } from "@/animations/displayAnimation"
 import { ProductCategory, ProductsFilterCard } from "./_components/filterCard"
 import { ProductsSection } from "./_components/products"
+import { Suspense } from "react"
 
 
 export interface Product {
@@ -71,7 +72,7 @@ export default async  function BLogPage (){
 return     <div className="flex flex-col py-10 px-2 max-w-full gap-6 items-center "  >
         <Title  titleClassName="text-primaryDark"  title="Nuestros Productos" descreption="Explora nuestra diversa selección de artículos de alta calidad para tu hogar." />
          <div className="flex px-10 py-5 flex-col gap-6" >
-        <ProductsHero/>
+         <Suspense> <ProductsHero/></Suspense>
         <DisplayAnimation className="w-full " delay={0.3} >
         <div className="flex flex-col lg:flex-row w-full  justify-between  gap-4 " >
         <ProductsFilterCard category={ undefined} />
