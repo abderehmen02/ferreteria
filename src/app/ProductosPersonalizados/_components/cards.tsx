e562c43b7212d5d038a23ce587e098f107217940
abderrahmane <ilahalaha0987@gmail.com>
2024-07-07 13:39:17 +0100

    personelized products is responsive

--- src/app/ProductosPersonalizados/_components/cards.tsx
+++ src/app/ProductosPersonalizados/_components/cards.tsx
@@ -38,19 +38,13 @@ const ProductSmallRectangleCard : React.FC<PersonelizedProductInfo>  = ({desc , 
 
 export const ProductLargeRectangleCard : React.FC<PersonelizedProductInfo>  = ({desc , image , title})=>{
      
-    //  const image = blogPost.image
-    // const extractedImgSrc = typeof image === "string" ? image.slice(0 , image.length - 5) : appConfig.notFountImage
-    // const imageSrcSet = typeof blogPost.image === "string" ? `${extractedImgSrc}-389.webp 389w , ${extractedImgSrc}-778.webp 778w` : ( blogPost.image389 && blogPost.image778) ?  `${sanityImageUrl(blogPost.image389 as object)} 389w , ${sanityImageUrl(blogPost.image778 as object)} 778w` : appConfig.notFountImage
-    // const hrefPath : string  =  blogPost.urlPath ? `/blog/${blogPost.urlPath}` :   '/blog/' + encodeURI( blogPost.isSanityPost ?  blogPost.title.replaceAll(" ", "-") : blogPost.title ) 
-    return <div className="flex rounded-xl items-center bg-gray-100  gap-6 p-6" >
-         <img alt={title} src={ image } sizes="389px" className="w-[638px] bg-white rounded-lg border-2 border-black p-4 h-[259px]" />
+    return <div className="flex rounded-xl items-center flex-col lg:flex-row bg-gray-100  gap-6 p-6" >
+         <img alt={title} src={ image } sizes="389px" className="lg:min-w-[508px] w-fit   bg-white rounded-lg border-2 border-black p-4 h-[259px]" />
          <div className=" gap-6 flex flex-col" >
         <div className="gap-4 flex flex-col" >
-        {/* <p className="pinkGradientText inline-block font-bold" >{new Date(blogPost.date)?.toString().slice(0 , 15)}</p> */}
          <h3 className="H3" >{title}</h3>
          <p   className="inline-block P"  >{desc}</p>
          </div>
-         {/* <Link href={hrefPath} ><p className="pinkGradientText font-bold" >Read More <i className="bi font-bold  bi-arrow-up-right"></i></p></Link> */}
          </div>
     </div>
 }
@@ -76,13 +70,13 @@ export const HeaderPersonelizedProducts  : React.FC= ()=>{
     {/* { facts[3] && <FactLargeRectangleCard {...facts[3]} />} */}
     </div>
 
-    {/* <div className="flex flex-col  relative gap-10  px-4 lg:hidden" >
+    <div className="flex flex-col  relative gap-10  px-4 lg:hidden" >
     <h2 className=" lg:hidden " >Recent <span className="pinkText" >Blog Posts</span></h2>
-    <div className="flex flex-col  gap-6" >
+    <div className="flex flex-col lg:hidden  gap-6" >
     {
-        allPersonlizedProducts.slice(0 , 4).map(post=>{
-        return <PostCard urlPath={post.urlPath} isBlogPage={true} lazyLoadImages={true} largeImg={post.largeImg} mediumImg={post.mediumImg} smallImg={post.smallImg} blurDataUrlImage={post.blurDataUrlImage}   date={post.date} key={post.id} displayDesktop={post.HomeDesktop} displayMobile={post.HomeMobile} title={post.title} descreption={extractedDescription} isSanityPost={post.isSanityPost} image={post.image} alt={post.imageAlt} />})
+        allPersonlizedProducts.slice(0 , 4).map(product=>{
+        return <ProductLargeRectangleCard key={product.title} {...product} />})
     }</div> 
-    </div> */}
+    </div>
     </>
 }
