bc6e4512fb89a4d5af74193a162994755918c54a
abderrahmane <ilahalaha0987@gmail.com>
2024-07-07 17:29:08 +0100

    integrating some products

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -62,7 +62,87 @@ image : "/images/beachChairs.jpeg" ,
     description : "Experimenta la máxima comodidad con nuestros calcetines de alta calidad, que ofrecen durabilidad y un ajuste perfecto para todo el día en cualquier actividad." ,
     image : "/images/socks.jpeg" ,
     category : "Calcetines"
-} ]
+} ,
+
+{
+    category: "Sombrillas",
+    name: "Sombrilla de Patio",
+    description: "Disfruta de tu espacio al aire libre con nuestra sombrilla de patio, diseñada para proporcionar sombra amplia y protección UV en tu jardín o terraza.",
+    image: "/images/umbrella4.jpeg"
+}, {
+    category: "Sombrillas",
+    name: "Sombrilla de Playa con Estilo",
+    description: "Asegura un día perfecto en la playa con nuestra sombrilla de playa con estilo, ofreciendo una sombra confiable y fácil de transportar.",
+    image:"/images/umbrella2.jpeg"
+}, {
+    category: "Sombrillas",
+    name: "Sombrilla de Jardín",
+    description: "Protege tus plantas y a ti mismo del sol con nuestra sombrilla de jardín, ideal para cualquier área verde.",
+    image:"/images/umbrella3.jpeg"
+}, {
+    category: "Sombrillas",
+    name: "Sombrilla Portátil",
+    description: "Lleva la sombra contigo a cualquier lugar con nuestra sombrilla portátil, perfecta para picnics, campamentos y eventos al aire libre.",
+    image: "/images/umbrella4.jpeg"
+}, {
+    category: "Sombrillas",
+    name: "Sombrilla de Terraza",
+    description: "Añade un toque de elegancia a tu terraza con nuestra sombrilla de terraza, que combina funcionalidad y estilo para tus reuniones al aire libre.",
+    image: "/images/umbrella5.jpeg"
+} , 
+{
+    category: "Flotadores",
+    name: "Flotador de Piscina",
+    description: "Disfruta de la piscina con nuestro flotador de piscina, diseñado para proporcionar comodidad y diversión en el agua.",
+    image: "/images/Flotadores1.jpeg"
+}, {
+    category: "Flotadores",
+    name: "Flotador Inflable para Niños",
+    description: "Mantén a los niños seguros y entretenidos con nuestro flotador inflable, perfecto para horas de diversión en la piscina.",
+    image: "/images/Flotadores2.jpeg"
+}, {
+    category: "Flotadores",
+    name: "Flotador de Anillo",
+    description: "Relájate en el agua con nuestro flotador de anillo, ideal para piscinas, lagos y playas.",
+    image: "/images/Flotadores3.jpeg"
+}, {
+    category: "Flotadores",
+    name: "Flotador de Flamenco",
+    description: "Añade un toque de estilo a tus días de piscina con nuestro flotador de flamenco, un accesorio divertido y llamativo.",
+    image: "/images/Flotadores4.jpeg"
+}, {
+    category: "Flotadores",
+    name: "Flotador de Bebidas",
+    description: "Mantén tus bebidas a flote y al alcance con nuestro flotador de bebidas, perfecto para fiestas en la piscina.",
+    image: "/images/Flotadores4.jpeg"
+} ,
+{
+    category: "Juguetes Acuáticos",
+    name: "Flotador Inflable",
+    description: "Diviértete en la piscina con nuestro flotador inflable, diseñado para brindar comodidad y diversión bajo el sol.",
+    image: "/images/Juguetes1.jpeg"
+}, {
+    category: "Juguetes Acuáticos",
+    name: "Pistola de Agua",
+    description: "Disfruta de emocionantes batallas acuáticas con nuestra pistola de agua, perfecta para niños y adultos en días calurosos.",
+    image: "/images/Juguetes2.jpeg"
+}, {
+    category: "Juguetes Acuáticos",
+    name: "Tabla de Surf para Niños",
+    description: "Inicia a tus hijos en el mundo del surf con nuestra tabla de surf para niños, diseñada para mayor estabilidad y seguridad.",
+    image: "/images/Juguetes3.jpeg"
+}, {
+    category: "Juguetes Acuáticos",
+    name: "Set de Buceo",
+    description: "Explora el mundo submarino con nuestro set de buceo, que incluye máscara, snorkel y aletas para una experiencia completa.",
+    image: "/images/Juguetes4.jpeg"
+}, {
+    category: "Juguetes Acuáticos",
+    name: "Pelota de Playa",
+    description: "Lleva la diversión a la playa o la piscina con nuestra colorida pelota de playa, ideal para juegos en el agua.",
+    image: "/images/Juguetes3.jpeg"
+}
+]
 
 
 export default async  function BLogPage (){
