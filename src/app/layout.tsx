0731d3fcbfcf1dbad04a2b36586e7a9b70e298d6
abderrahmane <ilahalaha0987@gmail.com>
2024-07-23 15:48:38 +0100

    fixing google ads issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,7 @@ export default function RootLayout({
   gtag('config', 'G-PEDPJLTJL0');`}
 </Script>       */}
 
+
  <Suspense>  <GAScripts /></Suspense>
 </head>
       <body  className={cn(roboto_Mono.variable , "flex items-center flex-col " )}>
